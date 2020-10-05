946ee88d8116a630b71129ff747cd2cf9937c5bf
waiizen <nathanferrer.nf@gmail.com>
2020-10-05 17:55:27 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+    $(".competence").css('cursor', 'pointer');
+    $(".competence").click(function (){
+        alert("okkk");
+    });
+    $(".box").css('line-height', ($(".box").css('height')))
+});
