f469933da12fa0d178cc92df0f0fbafc61c0a935
waiizen <nathanferrer.nf@gmail.com>
2020-10-06 12:25:28 +0200

    update finished the basic contentLeft finished the switch function

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 $(document).ready(function (){
+
+    // cache les div au chargement de la page
+    $('.competenceAll').hide();
+
+    // click sur competence
     $(".competence").css('cursor', 'pointer');
     $(".competence").click(function (){
-        alert("okkk");
+        $('.contentLeft').children().hide("slow");
+        $('.competenceAll').show("slow");
     });
+
+    // click sur formation
+    $(".formation").css('cursor', 'pointer');
+    $(".formation").click(function (){
+        $('.contentLeft').children().hide("slow");
+        $('.aboutAll').show("slow");
+    });
+
+    // calcul auto du line height
     $(".box").css('line-height', ($(".box").css('height')))
 });
